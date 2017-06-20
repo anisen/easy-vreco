9c5960827d311cf427d06bdcb275a07b81228776
anisen <ananavelez@gmail.com>
2017-06-20 13:07:43 -0400

    intente hacer algo

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+function initMap(){
+	var map= new google.maps.Map(document.getElementById("map"),{
+		zoom:5,
+		center:{lat:-9.1191427, lng: -77.0349046},
+		mapTypeControl:false,
+		zoomControl:false,
+		streetViewControl:false
+	});
+
+	function buscar(){
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+		}
+	}
+	document.getElementById("encuentrame").addEventListener("click",buscar);
+	var latitud,longitud;
+
+	var funcionExito= function(posicion){
+		latitud= posicion.coords.latitude;
+		longitud= posicion.coords.longitude;
+
+		var miUbicacion= new google.maps.Marker({
+			position: {lat:latitud, lng:longitud},
+			animation: google.maps.Animation.DROP,
+			map:map
+		});
+		map.setZoom(17);
+		map.setCenter({lat:latitud, lng:longitud});
+	}
+	var funcionError=function(error){
+		alert("Tenemos un problema con encontrar tu ubicacion");
+	};
+	var defaultBounds= new google.maps.LatLngBounds(
+		new google.maps.LatLng(-33.8902, 151.1759),
+		new google.maps.LatLng(-33.8474, 151.2631));
+
+	var options={
+		bounds:defaultBounds
+	};
+	var input=document.getElementById('pac-input');
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+	var autocomplete= new google.maps.places.autocomplete(input, options);
+}
+
